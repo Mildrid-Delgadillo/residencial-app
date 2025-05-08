e9dd61277784f2f583e7f299293e3434b8df27c6
Admin <admin@iMac-de-Admin.local>
2025-05-08 12:18:35 -0600

    Updates

---
+++ precache-manifest.9dc75036babf792d641f434a43fbda00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d01d602c8916f1eb84a5c5896c62b36d",
+    "url": "/residencial-app/index.html"
+  },
+  {
+    "revision": "d8979c8e78f60d4dbf62",
+    "url": "/residencial-app/static/css/main.ab3b9b02.chunk.css"
+  },
+  {
+    "revision": "17f4f83dc50538d959f2",
+    "url": "/residencial-app/static/js/2.4bb6ab0d.chunk.js"
+  },
+  {
+    "revision": "ba71f4f2c925f1d7c5ee",
+    "url": "/residencial-app/static/js/3.9093b10f.chunk.js"
+  },
+  {
+    "revision": "d8979c8e78f60d4dbf62",
+    "url": "/residencial-app/static/js/main.21973ba8.chunk.js"
+  },
+  {
+    "revision": "3cd9c03118728ab28887",
+    "url": "/residencial-app/static/js/runtime~main.4fa2b2e8.js"
+  }
+]);
